e2cb78791dc7ecf2e538992170253081115f1a00
ge-arty <george.arutinyan@gmail.com>
2023-07-08 15:39:32 +0400

    path update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/simple-react-router-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
